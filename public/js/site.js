00568c2402ce31961a6df3e51ea82fefab2cfd96
Felix Bygdemark <felixbygdemark@Felixs-MacBook-Pro.local>
2026-02-07 19:43:30 +0100

    Update CSS/JS

--- public/js/site.js
+++ public/js/site.js
@@ -1113,7 +1113,7 @@ function initBackgroundZoom() {
       defaults: { ease: "none" },
       scrollTrigger: {
         trigger: containers[0].querySelector("[data-bg-zoom-start]") || containers[0],
-        start: "clamp(top center)", // Change to "center center" to start from center of [data-bg-zoom-start]
+        start: "clamp(top bottom)", // Change to "center center" to start from center of [data-bg-zoom-start]
         endTrigger: containers[containers.length - 1],
         end: "bottom top",
         scrub: true,
@@ -1140,7 +1140,7 @@ function initBackgroundZoom() {
       // Part 1 - Move from Start to End position
       const zoomScrollRange = getScrollRange({
         trigger: startEl,
-        start: "clamp(top center)", // Change to "center center" to start from center of [data-bg-zoom-start]
+        start: "clamp(top bottom)", // Change to "center center" to start from center of [data-bg-zoom-start]
         endTrigger: endEl,
         end: "center center"
       });
