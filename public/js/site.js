cec4835bf7d761ab6d74dbbd234628ce292989b4
Felix Bygdemark <felixbygdemark@Felixs-MacBook-Pro.local>
2026-09-08 09:06:34 +0200

    Update CSS/JS

--- public/js/site.js
+++ public/js/site.js
@@ -96,12 +96,22 @@ function runPageLeaveAnimation(current, next) {
     onComplete: () => { current.remove() }
   });
   
+  const panel = nextPage.querySelector('[data-transition-panel]');
+  
   if (reducedMotion) {
     // Immediate swap behavior if user prefers reduced motion
     return tl.set(current, { autoAlpha: 0 });
   }
 
   tl.to(current, { y: 500, duration: 0.4 });
+
+  if (panel) {
+    tl.fromTo(panel, { 
+      yPercent: 100,
+    },{
+      yPercent: 0,
+    }, "startEnter");
+  }
 
   return tl;
 }
