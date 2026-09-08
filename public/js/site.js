61611773906cd88a3ac91d6e668866139973acfd
Felix Bygdemark <felixbygdemark@Felixs-MacBook-Pro.local>
2026-09-08 11:30:02 +0200

    Update CSS/JS

--- public/js/site.js
+++ public/js/site.js
@@ -105,9 +105,9 @@ function runPageLeaveAnimation(current, next) {
     return tl.set(current, { autoAlpha: 0 });
   }
 
-  tl.to(current, { y: 100, opacity: 0.9, scale: 0.9, duration: 0.8, ease: "power3.inOut" });
+  tl.to(current, { y: 50, opacity: 0.9, scale: 0.95, duration: 0.8, ease: "power3.inOut" });
 
-  tl.fromTo(panel, { yPercent: 100, y: 0 }, { yPercent: 0, duration: 0.8, ease: "power3.out" }, "<+=0.3");
+  tl.fromTo(panel, { yPercent: 100, y: 0 }, { yPercent: 0, duration: 0.6, ease: "power3.out" }, "<+=0.3");
 
   return tl;
 }
