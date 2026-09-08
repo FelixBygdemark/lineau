c5836a9f7eaac687d6b428a16b187c3f391b7a2d
Felix Bygdemark <felixbygdemark@Felixs-MacBook-Pro.local>
2026-09-08 09:23:13 +0200

    Update CSS/JS

--- public/js/site.js
+++ public/js/site.js
@@ -21,7 +21,7 @@ rmMQ.addListener?.(e => (reducedMotion = e.matches));
 const has = (s) => !!nextPage.querySelector(s);
 
 let staggerDefault = 0.05;
-let durationDefault = 1.5;
+let durationDefault = 1.1;
 
 CustomEase.create("osmo", "0.625, 0.05, 0, 1");
 gsap.defaults({ ease: "osmo", duration: durationDefault });
