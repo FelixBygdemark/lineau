6abcdc24a626e27e1bbd8a24d438ab3974fc0fcf
Felix Bygdemark <felixbygdemark@Felixs-MacBook-Pro.local>
2026-09-12 08:44:01 +0200

    Update CSS/JS

--- public/js/site.js
+++ public/js/site.js
@@ -130,7 +130,6 @@ function runPageLeaveAnimation(current, next) {
 
   tl.to(scaleContainer, {
      scale: 0.95,
-     y: "5vh",
      opacity: 0.7,
      duration: 0.8,
      ease: "power3.inOut"
